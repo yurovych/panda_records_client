2bf0f81412a6de62c48ce6969e005ef77c9a0a1e
volodymyr <volodiayurovych@gmail.com>
2025-01-06 21:04:46 +0100

    save

--- src/helpers/getTime.ts
+++ src/helpers/getTime.ts
@@ -3,11 +3,11 @@ import { UserMessageType } from '../types/UserMessage';
 export function getTime(message: UserMessageType) {
   const dateObject = new Date(message.created_at);
 
-  const day = dateObject.getDate();
-  const month = dateObject.getMonth();
+  const day = dateObject.getDate().toString().padStart(2, '0');
+  const month = (dateObject.getMonth() + 1).toString().padStart(2, '0');
   const year = dateObject.getFullYear();
-  const hours = dateObject.getHours();
-  const minutes = dateObject.getMinutes();
+  const hours = dateObject.getHours().toString().padStart(2, '0');
+  const minutes = dateObject.getMinutes().toString().padStart(2, '0');
 
-  return `${day}.${month + 1}.${year} / ${hours}:${minutes}`;
+  return `${day}.${month}.${year} / ${hours}:${minutes}`;
 }
--- src/modules/NotFoundPage/component/NotFoundPage.tsx
+++ src/modules/NotFoundPage/component/NotFoundPage.tsx
@@ -8,32 +8,34 @@ export const NotFoundPage: React.FC = () => {
   const { t } = useTranslation();
 
   return (
-    <div className={styles.notFoundPage}>
-      <div className={styles.notFoundPage__error}>
-        <h1 className={styles.notFoundPage__4}>4</h1>
-        <div className={styles.notFoundPage__imgBlock}>
-          <img
-            className={styles.notFoundPage__logo}
-            src='./icons/logo_panda52x52.svg'
-            alt='logo-ico'
-          />
+    <div className={styles.notFoundPageWrapper}>
+      <div className={styles.notFoundPage}>
+        <div className={styles.notFoundPage__error}>
+          <h1 className={styles.notFoundPage__4}>4</h1>
+          <div className={styles.notFoundPage__imgBlock}>
+            <img
+              className={styles.notFoundPage__logo}
+              src='./icons/logo_panda52x52.svg'
+              alt='logo-ico'
+            />
 
-          <img
-            className={styles.notFoundPage__oops}
-            src='./icons/oops.png'
-            alt='oops-ico'
-          />
+            <img
+              className={styles.notFoundPage__oops}
+              src='./icons/oops.png'
+              alt='oops-ico'
+            />
+          </div>
+
+          <h1 className={styles.notFoundPage__4}>4</h1>
         </div>
 
-        <h1 className={styles.notFoundPage__4}>4</h1>
-      </div>
+        <h2 className={styles.notFoundPage__text}>{t('not_found')}</h2>
 
-      <h2 className={styles.notFoundPage__text}>{t('not_found')}</h2>
-
-      <div className={styles.notFoundPage__button}>
-        <Link to={'/'}>
-          <Button text={t('return_to_home_page')} />
-        </Link>
+        <div className={styles.notFoundPage__button}>
+          <Link to={'/'}>
+            <Button text={t('return_to_home_page')} />
+          </Link>
+        </div>
       </div>
     </div>
   );
